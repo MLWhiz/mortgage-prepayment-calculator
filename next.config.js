bb3b253864b8fb131341ac126ad9a52004be2454
Rahul Agarwal <ragarwal@roku.com>
2025-05-05 23:32:52 +0100

    added more

---
+++ next.config.js
@@ -0,0 +1,7 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  reactStrictMode: true,
+  // Copy any other configuration from your next.config.ts file here
+};
+
+module.exports = nextConfig;
